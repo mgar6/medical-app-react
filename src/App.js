4ee68b608df1b93a699e7b113e16a63d3b8a90b9
Maya Garcia <mayagar6@gmail.com>
2023-11-26 06:47:08 -0500

    landing page component js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar/Navbar';
+import LandingPage from './Components/LandingPage/LandingPage';
 function App() {
 
   return (
@@ -10,6 +10,7 @@ function App() {
         <BrowserRouter>
           <Navbar/>
               <Routes>
+                <Route path='/' element={<LandingPage/>}/>
               </Routes>
             
         </BrowserRouter>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
     return (
         <nav>
         <div className="nav__logo">
-          <a href="../Landing_Page/LandingPage.html">StayHealthy <image src="https://cdn4.iconfinder.com/data/icons/medical-14/512/9-1024.png" height="35" width="35" viewBox="0 0 1000 1000" /><title>Doctor With Stethoscope SVG icon</title></a>
+          <a href="../LandingPage/LandingPage.html">StayHealthy <img src="https://cdn4.iconfinder.com/data/icons/medical-14/512/9-1024.png" height="35" width="35" viewBox="0 0 1000 1000" /><title>Doctor With Stethoscope SVG icon</title></a>
           {/* tenía style="fill:#3685fb" pero sale el error: the `style` prop expects a mapping from style properties to values, not a string. For example, style={{marginRight: spacing + 'em'}} when using JSX. */} 
         </div>
         <ul className="nav__links active">
