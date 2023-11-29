e4913670e8c12a21d43a4ad18881c6a198bbd241
Maya Garcia <mayagar6@gmail.com>
2023-11-29 08:05:43 -0500

    unfinished notification

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LandingPage from './Components/LandingPage/LandingPage';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import Notification from './Components/Notification/Notification';
 
 function App() {
 
@@ -17,7 +18,8 @@ function App() {
                 <Route path='/' element={<LandingPage/>}/>
                 <Route path="/instant-consultation" element={<InstantConsultation />} />
                 <Route path='/login' element={<Login/>}/> 
-                <Route path='/signup' element={<Sign_Up/>}/> 
+                <Route path='/signup' element={<Sign_Up/>}/>
+                <Route path='/notification' element={<Notification/>}/> ´{/* not working */}
               </Routes>
         </BrowserRouter>
        
