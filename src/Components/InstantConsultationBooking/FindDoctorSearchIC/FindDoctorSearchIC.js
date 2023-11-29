1bba91f195cfd58a8e0c82e6056e9a57a0234c06
Maya Garcia <mayagar6@gmail.com>
2023-11-29 06:39:33 -0500

    search icon

--- src/Components/InstantConsultationBooking/FindDoctorSearchIC/FindDoctorSearchIC.js
+++ src/Components/InstantConsultationBooking/FindDoctorSearchIC/FindDoctorSearchIC.js
@@ -24,14 +24,14 @@ const FindDoctorSearchIC = () => {
                 <h1>Find a doctor at your own ease</h1> {/*texto original: Find a doctor and Consult instantly */}
                 <div> 
                     <img src= './images/online-doctor.png' />             
-                    {/* <i style={{color:'#000000',fontSize:'20rem'}} className="fa fa-user-md"></i> */}
+                    {/*icono original <i style={{color:'#000000',fontSize:'20rem'}} className="fa fa-user-md"></i> */}
 </div>                <div className="home-search-container"  style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
                     <div className="doctor-search-box">
                     {/* <p>Perform a search to see the results.</p> */}
 
                         <input type="text" className="search-doctor-input-box" placeholder="Search doctors, clinics, hospitals, etc." onFocus={() => setDoctorResultHidden(false)} onBlur={() => setDoctorResultHidden(true)} value={searchDoctor} onChange={(e) => setSearchDoctor(e.target.value)} />
                         
-                        <div className="findiconimg"><img className='findIcon' src={process.env.PUBLIC_URL + '/images/search.svg'} alt=""/></div>
+                        <div className="findiconimg"><img className='findIcon' src={process.env.PUBLIC_URL + '/images/search.svg'} alt="" style={{height:"20px", width:"20px", marginTop:"8px"}} /></div>
                         <div className="search-doctor-input-results" hidden={doctorResultHidden}>
                             {
                                 specialities.map(speciality => <div className="search-doctor-result-item" key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
