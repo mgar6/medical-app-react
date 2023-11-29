84167fb5cd74ee0efbaa694d7060798ea44ebfba
Maya Garcia <mayagar6@gmail.com>
2023-11-29 07:38:07 -0500

    appointment form changes

--- src/Components/InstantConsultationBooking/AppointmentFormIC/AppointmentFormIC.js
+++ src/Components/InstantConsultationBooking/AppointmentFormIC/AppointmentFormIC.js
@@ -38,6 +38,22 @@ const AppointmentFormIC = ({ doctorName, doctorSpeciality, onSubmit }) => {
             required
           />
         </div>
+        <div className="form-group">
+          <label htmlFor="appointmentDate">Date of Appointment:</label>
+          <input
+            type="date"
+            id="appointmentDate"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="timeSlot">Book time slot:</label>
+          <select className="time-slot" value={selectedSlot} onChange={(e) => setSelectedSlot(e.target.value)}>
+                <option defaultValue>Select a time slot</option>
+                <option value="morning" name="morning">Morning</option>
+                <option value="afternoon" name="afternoon">Afternoon</option>
+        </select>
+        </div>
         <button className='book-appointment-btn' type="submit">Book Now</button>
       </form>
     );
