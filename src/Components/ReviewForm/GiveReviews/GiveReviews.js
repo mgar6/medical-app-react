41993b75c333df73b6697c2512d6504361e812ac
Maya <mayagar6@gmail.com>
2023-12-19 06:14:05 -0500

    Reviews list update

--- src/Components/ReviewForm/GiveReviews/GiveReviews.js
+++ src/Components/ReviewForm/GiveReviews/GiveReviews.js
@@ -1,61 +1,55 @@
 import React, { useState } from 'react';
 
 function GiveReviews() {
-  const [showForm, setShowForm] = useState(false);
-  const [submittedMessage, setSubmittedMessage] = useState('');
-  const [showWarning, setShowWarning] = useState(false);
-  const [formData, setFormData] = useState({
+    const [showForm, setShowForm] = useState(false);
+    const [submittedMessage, setSubmittedMessage] = useState('');
+    const [showWarning, setShowWarning] = useState(false);
+    const [formData, setFormData] = useState({
         name: '',
         review: '',
         rating: 0
-      });
+    });
 
-  const handleButtonClick = () => {
-    setShowForm(true);
-  };
+    const handleButtonClick = () => {
+        setShowForm(true);
+    };
 
-  const handleChange = (e) => {
-    setFormData(e.target.value);
-  };
+    const handleChange = (e) => {
+        setFormData(e.target.value);
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSubmittedMessage(formData);
-    setFormData('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmittedMessage(formData);
+        setFormData('');
         if (formData.name && formData.review && formData.rating > 0) {
-          setShowWarning(false);
+            setShowWarning(false);
         } else {
-          setShowWarning(true);
+            setShowWarning(true);
         }
-  };
-  return (
-    <div>
-      <h2>Form with Message</h2>
-      {!showForm ? (
-        <button onClick={handleButtonClick}>Open Form</button>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <h2>Give Your Feedback</h2>
-               {showWarning && <p className="warning">Please fill out all fields.</p>}
-                <div>
-                   <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
-             </div>
-                <div>
-                 <label htmlFor="review">Review:</label>
-                  <textarea id="review" name="review" value={formData.review} onChange={handleChange} />
-                 </div>
-                 <button type="submit">Submit</button>
-               </form>
-      )}
-      {submittedMessage && (
+    };
+    return (
         <div>
-          <h3>Submitted Message:</h3>
-          <p>{submittedMessage}</p>
+            <form onSubmit={handleSubmit}>
+                <h2>Give Your Feedback</h2>
+                {showWarning && <p className="warning">Please fill out all fields.</p>}
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="review">Review:</label>
+                    <textarea id="review" name="review" value={formData.review} onChange={handleChange} />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+            {submittedMessage && (
+                <div>
+                    <h3>Submitted Message:</h3>
+                    <p>{submittedMessage}</p>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
-
 export default GiveReviews;
--- src/Components/ReviewForm/ReviewForm.js
+++ src/Components/ReviewForm/ReviewForm.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect} from 'react';
 import './ReviewForm.css';
+import Popup from 'reactjs-popup';
+import GiveReviews from './GiveReviews/GiveReviews';
+
 
 const ReviewForm = () => {
-    const [doctors, setDoctors] = useState([]);
+    const [allDoctors, setAllDoctors] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    //const [counter, setCounter] = useState(0);
+    let count = 0;
     
     const getDoctorsNames = () => {
         fetch('https://api.npoint.io/9a5543d36f1460da2f63')
         .then(res => res.json())
         .then(data => {
-            setDoctors(data);
-            //console.log("Doctor names: " + JSON.stringify(data));
+            setAllDoctors(data);
             console.log("Doctor names: " + data.map(doctor => doctor.name));
         })
         .catch(err => console.log(err));
@@ -32,7 +37,30 @@ const ReviewForm = () => {
                     </tr>
                 </thead>
                 <tbody>
-
+                    {allDoctors.map( doctor => (
+                        
+                        <tr className="table-row">
+                            <td>{count = count+1}</td> {/* setCounter(counter+1)*/}
+                            <td>{doctor.name}</td>
+                            <td>{doctor.speciality}</td>
+                            <td>
+                                <Popup
+                                    style={{ backgroundColor: '#FFFFFF' }}
+                                    trigger={
+                                        <button className="review-button">Give Review</button>
+                                    }
+                                    modal
+                                    open={showModal}
+                                    onClose={() => setShowModal(false)}
+                                >
+                                    {(close) => (
+                                        <GiveReviews/>
+                                    )}
+                                </Popup> 
+                            </td>
+                            <td></td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
